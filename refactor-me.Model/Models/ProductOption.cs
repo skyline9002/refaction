967893c5ea58767aecb8a18d13b047e72dad8e1f
Hai Huang <simonhuang211@gmail.com>
2017-10-06 21:49:30 +1300

    scaffold the solution to Model, Data, Service, Api, add Models

---
+++ refactor-me.Model/Models/ProductOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refactor_me.Model.Models
+{
+    public class ProductOption
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public  string Description { get; set; }
+        
+    }
+}
